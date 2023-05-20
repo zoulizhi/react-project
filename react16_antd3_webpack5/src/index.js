e478f559eebad702e8a8a9f79e3f86fdd59db541
zoulizhi <306360433@qq.com>
2023-05-20 13:17:58 +0800

    打包其他资源

--- react16_antd3_webpack5/src/index.js
+++ react16_antd3_webpack5/src/index.js
@@ -1,7 +1,22 @@
 import "./style.css";
 import "./style.less";
-console.log(555);
+import "../public/assets/font/iconfont.css";
+const zznhImg = "../public/assets/img/zznh.png";
 let root = document.getElementById("root");
 let divDom = document.createElement("div");
 divDom.innerHTML = "9999";
 root.append(divDom);
+
+const zznhImage = new Image();
+zznhImage.src = zznhImg;
+root.appendChild(zznhImage);
+
+const bgDiv = document.createElement("div");
+bgDiv.style.width = "200px";
+bgDiv.style.height = "200px";
+bgDiv.className = "bg-image";
+root.append(bgDiv);
+
+const iEl = document.createElement("i");
+iEl.className = "iconfont icon-ashbin";
+root.append(iEl);
--- react16_antd3_webpack5/webpackConfig/dev.js
+++ react16_antd3_webpack5/webpackConfig/dev.js
@@ -22,6 +22,31 @@ module.exports = {
         test: /\.less$/,
         use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
       },
+      /**
+       * 打包图片资源，小图片转成base64之后可以和页面一起被请求，减少不必要的请求过程
+       */
+      {
+        test: /\.(png|svg|jpg|jpge|gif)$/i,
+        type: "asset",
+        generator: {
+          filename: "img/[name].[hash:6][ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024,
+          },
+        },
+      },
+      /**
+       * 字体的打包
+       */
+      {
+        test: /\.(woff2?|eot|ttf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "font/[name].[hash:6][ext]",
+        },
+      },
     ],
   },
   plugins: [
